2f2c64850706c4c030a2d7de5ba13a3ab25d1519
NicoPrg <nicoprogramas1234@gmail.com>
2024-05-16 15:56:18 -0300

    Update teste_mapa.py

--- teste_mapa.py
+++ teste_mapa.py
@@ -4,35 +4,28 @@
 # pip install pandas
 # pip install geopy
 
-
 import folium
 import folium.plugins
 import geopandas as gpds
 import pandas as pds
 
+#transformando a localização
+end = input("Endereço: ") # R. Manoel Santos Chieira, 92
+coord = gpds.tools.geocode(end, provider = "nominatim", user_agent = "myGeocode")["geometry"]  # só funciona na janela interativa
+string = str(coord[0])
+separacao = string.split()
+separacao.remove(separacao[0])
+lat = (separacao[0].replace('(',''))
+lon = (separacao[1].replace(')',''))
+print(lat, lon)
 
-# transformando a localização
-#end = input("Endereço: ") # R. Manoel Santos Chieira, 92
-#coord = gpds.tools.geocode(end, provider = "nominatim", user_agent = "myGeocode")["geometry"]  # só funciona na janela interativa
-#df = pds.DataFrame({coord[0]})
-#df[0:1]
-#df
-#lista = []
-#lista = str(coord).split(" ")
-#print(lista)
-## configurações do mapa
-##lat, lon = input('Digite a localização: ').split(',')
-#m = folium.Map(location=(-22.2127829,-49.9557924), zoom_start = 12, control_scale = True, )
-#folium.plugins.Geocoder().add_to(m)
-#folium.plugins.Fullscreen(position="topright", title="Expand me", title_cancel="Exit me", force_separate_button=True, ).add_to(m)
-#
-## marcadores
-#folium.Marker(location = [coord], tooltip = 'RyuLol', popup="Que cara foda", icon=folium.Icon(color=""), ).add_to(m)
-#
-## start
-#m
-dados={'nome':['ana', 'bruno', 'carlos', 'diana'], 'idade':[23,34,45,26],'cidade':['sao paulo', 'reio de janeiro', 'salvador','curitiba']}
-df=pds.DataFrame(dados)
+# configurações do mapa
+m = folium.Map(location=(-22.2127829,-49.9557924), zoom_start = 12, control_scale = True, )
+folium.plugins.Geocoder().add_to(m)
+folium.plugins.Fullscreen(position="topright", title="Expand me", title_cancel="Exit me", force_separate_button=True, ).add_to(m)
 
+# marcadores
+folium.Marker(location = [lat, lon]).add_to(m)
 
-print(df['nome'])
+# start
+m
