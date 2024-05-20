0e59e70270d9c5b38a8e1551e4dfb32ff6e1f52f
NicoPrg <nicoprogramas1234@gmail.com>
2024-05-20 16:48:10 -0300

    .

--- Flask.py
+++ Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #iniciando
 app = Flask(__name__)
@@ -6,7 +6,8 @@ app = Flask(__name__)
 #rotas
 @app.route('/')
 def login():
-    return 
+    return render_template("login.html") 
 
 #execução
-app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug = True)
--- mapa/mapa_interativo.py
+++ mapa/mapa_interativo.py
@@ -14,16 +14,6 @@ import geopandas as gpds
 import pandas as pds
 import mysql.connector
 
-#Conectando com banco de dados
-def conectar_banco():
-    return mysql.connector.connect(
-        host='localhost', 
-        user = 'root', 
-        password = '', 
-        database = 'pins', 
-        charset = 'utf8'
-    )
-
 #Pegando a localização
 end = input("Endereço: ") # R. Manoel Santos Chieira, 92
 coord = gpds.tools.geocode(end, provider = "nominatim", user_agent = "myGeocode")["geometry"]  # só funciona na janela interativa
@@ -33,11 +23,6 @@ separacao.remove(separacao[0])
 lat = (separacao[1].replace(')',''))
 lon = (separacao[0].replace('(',''))
 print(lat, lon)
-con = conectar_banco()
-cursor = con.cursor()
-cursor.execute("insert into enderecos(rua, lat, lon)values(%s, %s, %s)",(end, lat, lon))
-con.commit()
-con.close()
 
 #Configurações do mapa
 m = folium.Map(location=(-22.2127829,-49.9557924), zoom_start = 12, control_scale = True, )
