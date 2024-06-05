97796679427cdd4a7aea4f3e1c99ba68f2f91053
NicoPrg <nicoprogramas1234@gmail.com>
2024-06-04 22:14:55 -0300

    .

--- app.py
+++ app.py
@@ -73,14 +73,20 @@ def mapa():
     import pandas as pds
 
     #Pegando a localização
-    end = request.form.get("end") # R. Manoel Santos Chieira, 92
+    end = request.form.get("end")# R. Manoel Santos Chieira, 92
     coord = gpds.tools.geocode(end, provider = "nominatim", user_agent = "myGeocode")["geometry"]  # só funciona na janela interativa
     string = str(coord[0])
     separacao = string.split()
     separacao.remove(separacao[0])
     lat = (separacao[1].replace(')',''))
     lon = (separacao[0].replace('(',''))
-    print(lat, lon)
+    
+    with open('localiza.json', 'r+') as f:
+       data = json.load(f)
+       data.append({"lat": lat, "lon": lon})
+       f.seek(0)
+       json.dump(data, f, indent=4)
+       f.truncate()
 
     #Configurações do mapa
     m = folium.Map(location=(-22.2127829,-49.9557924), zoom_start = 12, control_scale = True, )
@@ -88,7 +94,10 @@ def mapa():
     folium.plugins.Fullscreen(position="topright", title="Expand me", title_cancel="Exit me", force_separate_button=True, ).add_to(m)
 
     # Marcador
-    folium.Marker(location = [lat, lon]).add_to(m)
+    with open('localiza.json', 'r') as localiza:
+        lista = json.load(localiza)
+        for c in lista:
+            folium.Marker(location = [c['lat'], c['lon']]).add_to(m)
 
     # Rodando
     m.save("templates/mapa.html")
